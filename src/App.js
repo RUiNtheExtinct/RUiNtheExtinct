ab6bcf778c940abffedd4085c8454437e8713b77
RUiNtheExtinct <deepkarma001@gmail.com>
2021-09-06 09:18:58 +0530

    fuck gatsby

---
+++ src/App.js
@@ -0,0 +1,55 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import reportWebVitals from "./reportWebVitals";
+
+import React, { useState, useEffect } from "react";
+import Preloader from "./components/Pre";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+// import CP from "./views/CP/CP";
+import Experience from "./views/Experience/ExperiencePage";
+import Footer from "./components/Footer";
+import Resume from "./components/Resume/Resume";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserHistory } from "history";
+
+import ScrollToTop from "./components/ScrollToTop";
+
+var hist = createBrowserHistory();
+export default function App() {
+	const [load, upadateLoad] = useState(true);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			upadateLoad(false);
+		}, 1200);
+
+		return () => clearTimeout(timer);
+	}, []);
+
+	return (
+		<React.StrictMode>
+			<Router>
+				<Preloader load={load} />
+				<div className="App" id={load ? "no-scroll" : "scroll"}>
+					<Navbar />
+					<ScrollToTop />
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/project" component={Projects} />
+						<Route path="/resume" component={Resume} />
+						<Route path="/about" component={About} />
+						<Route path="/exp" component={Experience} />
+					</Switch>
+					<Footer />
+				</div>
+			</Router>
+		</React.StrictMode>
+	);
+}
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+// import {BrowserRouter } from "react-router-dom";
+
+import "./assets/scss/material-kit-react.scss?v=1.10.0";
+
+// pages for this product
+
+import App from "./App";
+var hist = createBrowserHistory();
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+reportWebVitals();
